fe3891a39a0d87c42e5d14b5724a52d4d6cf4a57
Fabrício Aranha <fabricioaranha1@gmail.com>
2022-05-05 21:38:02 -0300

    Updated Boss Movimentation

---
+++ README.md
@@ -0,0 +1,2 @@
+# projeto-ip-cc
+Projeto da disciplina de introdução a programação do curso de ciência da computação no CIn-UFPE
---
+++ sprite_mananger/sprite_mananger.py
@@ -0,0 +1,15 @@
+import pygame as pg 
+
+class SpriteManganger():
+    def __init__(self, image: pg.surface.Surface) -> None:
+        self.sheet = image
+
+    def get_image(self, frame: int, width: int, height, scale: float, bg_color) -> pg.surface.Surface:
+        
+        image = pg.Surface((width, height)).convert_alpha()
+        image.blit(self.sheet, (0,0), ((frame * width), 0, width, height))
+        image = pg.transform.scale(image, (width * scale, height * scale))
+        image.set_colorkey(bg_color)
+
+        return image
+    
